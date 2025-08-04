1060e7530042db6b8037085b9833a7f16c8b1a1f
ameerlouly <ameer.Louly04@eng-st.cu.edu.eg>
2025-08-05 00:08:11 +0300

    spi wrapper completed

--- Main(Wrapper).v
+++ Main(Wrapper).v
@@ -1,1 +1,29 @@
+module spi_wrapper(
+    input   MOSI,
+            SS_n,
+            clk,
+            rst_n;
+    output  MISO;
+);
+    wire rx_valid, tx_valid;
+    wire [9 : 0]    rx_data;
+    wire [7 : 0]    tx_data;
 
+    SPI_Slave slave(.MOSI(MOSI),
+                    .MISO(MISO),
+                    .SS_n(SS_n),
+                    .clk(clk),
+                    .rst_n(rst_n),
+                    .rx_data(rx_data),
+                    .rx_valid(rx_valid),
+                    .tx_data(tx_data),
+                    .tx_valid(tx_valid));
+                    
+    RAM ram(.din(rx_data),
+            .rx_valid(rx_valid),
+            .dout(tx_data),
+            .tx_valid(tx_valid),
+            .clk(clk),
+            .rst_n(rst_n));
+
+endmodule
